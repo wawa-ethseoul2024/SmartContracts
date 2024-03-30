d52eb434b306ec9645113e2bb1b66f61da03b505
Cosmodude <vladislav.lenskii@gmail.com>
2024-03-30 19:18:39 +0900

    wawProfile contract

---
+++ contracts/Profile.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract WawaProfile is ERC721, ERC721Burnable, Ownable {
+    uint256 private _nextTokenId;
+
+    constructor(address initialOwner)
+        ERC721("WawaProfile", "WAWA")
+        Ownable(initialOwner)
+    {}
+
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+    }
+}
