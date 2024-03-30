ed0a7a5cb832ad26791e4099d4ef56f266ed5dea
Cosmodude <vladislav.lenskii@gmail.com>
2024-03-30 19:26:08 +0900

    added description

--- contracts/Profile.sol
+++ contracts/Profile.sol
@@ -6,6 +6,11 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+/**
+ * @title Contract of the ETHSeoul2024 Hackathon
+ * @author @cosmodude
+ * @notice ERC721 token 
+ */
 contract WawaProfile is ERC721, ERC721Burnable, Ownable {
     uint256 private _nextTokenId;
 
